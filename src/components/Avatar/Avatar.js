e51e97ba7ed12ebb3953b992bc39b55d03ca0b38
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-08-26 18:20:56 +0700

    :art: use `Konva` instead of `img`

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,38 +1,76 @@
 import React from "react";
+import { Group, Image, Layer, Stage } from "react-konva";
 import "./Avatar.css";
 function Avatar({ selectedItems, handleRandomize }) {
+  const IMG_W = 250;
+  const IMG_H = 250;
   const accessories = ["earrings", "glasses", "hats", "neckwear"];
   const clothes = ["layer_1", "layer_2", "layer_3"];
-  let parts = [];
+
+  let imageArr = [];
   for (let i in selectedItems) {
+    let img = new window.Image();
     let tmpType = i;
     if (accessories.includes(tmpType)) {
       tmpType = `accessories\\${tmpType}`;
     } else if (clothes.includes(tmpType)) {
       tmpType = `clothes\\${tmpType}`;
     }
-
-    parts.push(
-      <img
-        src={`./character/${tmpType}/${selectedItems[i] + 1}.png`}
-        alt={`${i}/${selectedItems[i]}`}
-        height={100}
-        style={{ position: "absolute", zoom: "2.5" }}
-        key={i + selectedItems[i]}
-      />
-    );
+    img.src = `./character/${tmpType}/${selectedItems[i] + 1}.png`;
+    imageArr.push(img);
   }
+  const noseImg = new window.Image();
+  noseImg.src = `./character/noses/1.png`;
+  imageArr.push(noseImg);
+
   return (
     <div>
       <div className="avatar-wrapper">
         <div className="avatar">
-          {parts}
-          <img
-            src={`./character/noses/1.png`}
-            alt="noses"
-            height={100}
-            style={{ position: "absolute", zoom: "2.5" }}
-          />
+          <Stage width={IMG_W} height={IMG_H}>
+            <Layer>
+              <Group>
+                {imageArr
+                  .filter((i, index) => index < 5)
+                  .map((i, index) => (
+                    <Image
+                      key={i + index}
+                      height={IMG_H}
+                      width={IMG_W}
+                      image={i}
+                    />
+                  ))}
+              </Group>
+            </Layer>
+            <Layer>
+              <Group>
+                {imageArr
+                  .filter((i, index) => index >= 5 && index < 10)
+                  .map((i, index) => (
+                    <Image
+                      key={i + index}
+                      height={IMG_H}
+                      width={IMG_W}
+                      image={i}
+                    />
+                  ))}
+              </Group>
+            </Layer>
+            <Layer>
+              <Group>
+                {imageArr
+                  .filter((i, index) => index >= 10)
+                  .map((i, index) => (
+                    <Image
+                      key={i + index}
+                      height={IMG_H}
+                      width={IMG_W}
+                      image={i}
+                    />
+                  ))}
+              </Group>
+            </Layer>
+          </Stage>
         </div>
         <div>
           <button className="random-btn" onClick={handleRandomize}>
